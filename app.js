199101837196356f73bfb25ec0b7e3af13317b21
Ale Kennedy <alepertuz@gmail.com>
2023-03-08 16:29:01 -0500

    updated cors for deployment

--- app.js
+++ app.js
@@ -26,10 +26,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-   cors({
-     origin: [process.env.FRONTEND_URI]  // <== URL of our future React app
-   })
+   cors()
 );
+
+// app.use(
+//    cors({
+//      origin: [process.env.FRONTEND_URI]  // <== URL of our future React app
+//    })
+// );
 
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
